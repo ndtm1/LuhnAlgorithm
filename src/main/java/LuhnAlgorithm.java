1ed4731a48cde3b4adfaa02f204a996318d4841c
neydtm <degtyarevn5@gmail.com>
2023-07-16 23:17:56 +0300

    .

---
+++ src/main/java/LuhnAlgorithm.java
@@ -0,0 +1,104 @@
+public class LuhnAlgorithm {
+
+    public static void main(String[] args) {
+        String number = new LuhnAlgorithm().makeCheckDigit("553691402262641");
+        System.out.println(number);
+        System.out.println(new LuhnAlgorithm().verifyCheckDigit(number));
+    }
+
+    public String makeCheckDigit(String seqNumbers) {
+
+        int[] numbersToSave = fillArray(seqNumbers, false);
+        int[] numbers = fillArray(seqNumbers, false);
+
+        if (((numbers.length - 1) % 2) == 0) {
+
+            for (int i = 1; i < numbers.length - 1; i += 2)
+                numbers[i] = multiplyTwoAndSubtractIfOverNine(numbers[i]);
+
+            int finalDigit = getFinalDigit(numbers, false);
+            numbersToSave[seqNumbers.length()] = finalDigit;
+
+        } else {
+
+            for (int i = 0; i < numbers.length - 1; i += 2) {
+                numbers[i] = multiplyTwoAndSubtractIfOverNine(numbers[i]);
+
+                int finalDigit = getFinalDigit(numbers, false);
+                numbersToSave[seqNumbers.length()] = finalDigit;
+            }
+        }
+
+        return seqToString(numbersToSave);
+    }
+
+    public boolean verifyCheckDigit(String numberWithCheckDigit) {
+        int[] numbers = fillArray(numberWithCheckDigit, true);
+
+        if (((numbers.length - 1) % 2) == 0) {
+
+            for (int i = 1; i < numbers.length - 1; i += 2)
+                numbers[i] = multiplyTwoAndSubtractIfOverNine(numbers[i]);
+
+            int finalDigit = getFinalDigit(numbers, true);
+
+            return (finalDigit % 10) == 0;
+        } else {
+
+            for (int i = 0; i < numbers.length - 1; i += 2)
+                numbers[i] = multiplyTwoAndSubtractIfOverNine(numbers[i]);
+
+
+            int finalDigit = getFinalDigit(numbers, true);
+
+            return (finalDigit % 10) == 0;
+        }
+    }
+
+    private int[] fillArray(String seqNumbers, boolean verify) {
+        int[] numbers;
+
+        if(!verify) {
+            numbers = new int[seqNumbers.length() + 1];
+        } else
+            numbers = new int[seqNumbers.length()];
+
+        for(int i = 0; i < seqNumbers.length(); i++) {
+            numbers[i] = Integer.parseInt(String.valueOf(seqNumbers.charAt(i)));
+        }
+
+        return numbers;
+    }
+
+    private int multiplyTwoAndSubtractIfOverNine(int number) {
+        number = number * 2;
+
+        return (number > 9) ? number - 9 : number;
+    }
+
+    private int getFinalDigit(int[] numbers, boolean verify) {
+        int finalNumber = 0;
+
+        for(int i = 0; i < numbers.length - 1; i++)
+            finalNumber += numbers[i];
+
+        int temp = finalNumber;
+        if(!verify) {
+            finalNumber = (int) (Math.ceil(temp / 10f) * 10) - temp;
+        } else {
+            finalNumber = temp + numbers[numbers.length - 1];
+        }
+
+        return finalNumber;
+    }
+
+    private String seqToString(int[] numbers) {
+        StringBuilder sb = new StringBuilder();
+        for(int i : numbers) {
+
+            sb.append(i);
+        }
+
+        return sb.toString();
+    }
+}
---
+++ src/test/java/TestLuhn.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+
+public class TestLuhn {
+
+    @Test
+    public void testCorrectCheckDigit() {
+
+    }
+}
